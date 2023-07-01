d00dda7d2b6098cba971cc4f2a7919f0a525873c
rafijadika <jadika.rafi@gmail.com>
2023-07-01 19:44:46 +0700

    commit 11

--- binarysearchtree_007/binarysearchtree_007.cpp
+++ binarysearchtree_007/binarysearchtree_007.cpp
@@ -45,4 +45,11 @@ public:
 			return;
 		}
 
-		
+		if (element < parent->info)
+		{
+			parent->leftchild = newNode;
+		}
+		else if (element > parent->info)
+		{
+			parent->rightchild = newNode;
+		}
